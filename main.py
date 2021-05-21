09da41d6a8e4e6795cb144cf8d0e4737896a0296
PiotrBebel001 <PiotrBebel001@users.noreply.github.com>
2021-05-21 23:26:46 +0200

    wprowadznie do zmiennych 1 i 2

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hello world")
+
+#wprowadzenie do zmiennych 1
+imie = "Piotr"
+print("Hello World!" + imie)
+
+#wprowadzenie do zmiennych 2
+imie = "Wiktoria"
+ilosc_ksiazek = 10
+srednia = 4.5
+
+print("Hello world!" + imie)
+print("Ma " + str(ilosc_ksiazek))
+print("Srednia: " + str(srednia))

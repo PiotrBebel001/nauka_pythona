0d540b9dcf5c38fe8f3267bc8a467ba432f45700
PiotrBebel001 <PiotrBebel001@users.noreply.github.com>
2021-05-22 13:10:43 +0200

    metody dzielenia wartosci agrumentow

--- obliczenia.py
+++ obliczenia.py
@@ -1,2 +1,11 @@
+#dodawanie argumentow
 sum = (1 + 3)
-print(sum)
+print("Suma wynosi: ", sum)
+
+#dzielenie
+sum = (10 / 3)
+print("Wynik dzielenia wynosi: ", sum)
+sum = (10.0 / 3)
+print("Wynik dzielenia wynosi:", sum)
+sum = (float(10) / 3)
+print("Wynik dzielenia wynosi:", sum)

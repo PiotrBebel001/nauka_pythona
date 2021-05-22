760d8b1f22beebad499053db05b252db0127f18c
PiotrBebel001 <PiotrBebel001@users.noreply.github.com>
2021-05-22 13:37:05 +0200

    funkcje string

---
+++ funkcje_string.py
@@ -0,0 +1,9 @@
+marka = "Peugot"
+ilosc_drzwi = 5
+pojemnosc = 1.3
+
+marka_up = marka.upper()
+
+print("Samochod "  +  marka  +  " ma "  +   str(ilosc_drzwi)  +  " drzwi")
+print(marka_up)
+print("Pojemnosc po zmianach: " + str(pojemnosc * 2))

226ff59ef8b57150cfea1ede8bc8ba50f2507107
PiotrBebel001 <PiotrBebel001@users.noreply.github.com>
2021-05-22 13:23:54 +0200

    dodawanie zmiennych

--- obliczenia.py
+++ obliczenia.py
@@ -9,3 +9,14 @@ sum = (10.0 / 3)
 print("Wynik dzielenia wynosi:", sum)
 sum = (float(10) / 3)
 print("Wynik dzielenia wynosi:", sum)
+
+#wykorzystanie pythona jako kalkulator
+sum = (4000 / 4.3)
+print("Za 4000 PLN dostaninemy: ", sum, "euro")
+
+# 30%
+sum = (4000.0 * 33) / 100
+
+#zmienne tez dzialaja:
+cena_euro = (4000.0) / 4.3
+print("Za 4000 PLN dostaniemy:", cena_euro, "euro")
